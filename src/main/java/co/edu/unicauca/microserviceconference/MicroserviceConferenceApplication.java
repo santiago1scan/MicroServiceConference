f97633dfde53c66490fa6e115b5225fe2a7ceb19
Santiago <santiescandon1@gmail.com>
2024-10-17 16:35:21 -0500

    first commit

---
+++ src/main/java/co/edu/unicauca/microserviceconference/MicroserviceConferenceApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.microserviceconference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConferenceApplication.class, args);
+	}
+
+}
