a767778c7aba7b66ee6875c5584492a8956f4689
Santiago <santiescandon1@gmail.com>
2024-10-17 18:24:13 -0500

    implements models basic

---
+++ src/main/java/co/edu/unicauca/microserviceconference/domain/model/Article.java
@@ -0,0 +1,16 @@
+package co.edu.unicauca.microserviceconference.domain.model;
+
+public class Article {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Article() {
+    }
+}
--- src/main/java/co/edu/unicauca/microserviceconference/domain/model/Conference.java
+++ src/main/java/co/edu/unicauca/microserviceconference/domain/model/Conference.java
@@ -1,4 +1,72 @@
 package co.edu.unicauca.microserviceconference.domain.model;
 
+import java.util.Date;
+
 public class Conference {
+    private int id;
+    private String place;
+    private String basicInfo;
+    private String topics;
+    private Date date;
+    private User user;
+    private Article article;
+    public Conference() {
+
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public String getBasicInfo() {
+        return basicInfo;
+    }
+
+    public String getTopics() {
+        return topics;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Article getArticle() {
+        return article;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setArticle(Article article) {
+        this.article = article;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setTopics(String topics) {
+        this.topics = topics;
+    }
+
+    public void setBasicInfo(String basicInfo) {
+        this.basicInfo = basicInfo;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
