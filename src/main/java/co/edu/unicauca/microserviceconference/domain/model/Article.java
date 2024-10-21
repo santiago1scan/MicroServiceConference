62c83142a5ffeb0926c2fa99351285e2be3ea4a0
Santiago <santiescandon1@gmail.com>
2024-10-20 23:03:05 -0500

    Article V2 implements atribute

--- src/main/java/co/edu/unicauca/microserviceconference/domain/model/Article.java
+++ src/main/java/co/edu/unicauca/microserviceconference/domain/model/Article.java
@@ -1,16 +1,27 @@
 package co.edu.unicauca.microserviceconference.domain.model;
 
+import jakarta.websocket.RemoteEndpoint;
+
 public class Article {
-    private String id;
+    private String name;
+    private String idAuthor;
+    private String keywords;
+    private BasicDate publicationDate;
 
-    public String getId() {
-        return id;
-    }
+    public String getName() {return name;}
+    public String getIdAuthor() {return idAuthor;}
+    public String getKeywords() {return keywords;}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+    public BasicDate getPublicationDate() {return publicationDate;}
+    public void setName(String name) {this.name = name;}
+    public void setIdAuthor(String idAuthor) {this.idAuthor = idAuthor;}
+    public void setKeywords(String keywords) {this.keywords = keywords;}
+    public void setPublicationDate(BasicDate publicationDate) {this.publicationDate = publicationDate;}
 
-    public Article() {
+    public Article(String name, String idAuthor,String keywords, BasicDate publicationDate) {
+        this.name = name;
+        this.idAuthor = idAuthor;
+        this.keywords = keywords;
+        this.publicationDate = publicationDate;
     }
 }
